465dd488e70e3246f27592b87fe0adb482396167
Kaamil <kaamilwarsi@icloud.com>
2026-03-02 09:45:45 +0530

    Fixed an error where the computer will still prompt the user if he/she wants to take pictures even if the user is below 12 years of age (ineligible for ride).

--- rollercoaster.py
+++ rollercoaster.py
@@ -9,18 +9,18 @@ if height >= 130:
     age = int(input("What is your age? "))
     if age <= 11:
         print("Unfortunately, you are not eligible for a rollercoaster ride yet. We hope to see you once you turn 12 years old!")
-    if age < 18:
+    elif age < 18:
         print("Congratulations, you are eligible for a rollercoaster.\nPeople under 18 years pay $7")
         bill += 7
     if age >= 18:
         print("Congratulations, you are eligible for a rollercoaster.\nPeople over 18 years pay $12")
         bill += 12
 
-    picture = input("Do you want your pictures to be taken on the rollercoaster? (y/n)? ")
-    if picture == "y":
-        bill += 3
-        print(f"Alright, your total bill is ${bill}")
-    else:
-        print(f"Alright, your total bill is ${bill}")
+        picture = input("Do you want your pictures to be taken on the rollercoaster? (y/n)? ")
+        if (picture == "y"):
+            bill += 3
+            print(f"Alright, your total bill is ${bill}")
+        else:
+            print(f"Alright, your total bill is ${bill}")
 else:
         print("Unfortunately, you are not eligible for a rollercoaster. Our minimum height requirement is 130 centimeters.")
